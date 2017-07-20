8aee058474416e348f43cef73c80cecdd91069b5
Kuno Tatemaki <raulfeliz@gmail.com>
2017-07-20 18:54:22 +0200

    move main activity to activities package

--- app/src/main/java/com/rukiasoft/githubfetcher/ui/activities/GithubListActivity.java
+++ app/src/main/java/com/rukiasoft/githubfetcher/ui/activities/GithubListActivity.java
@@ -1,4 +1,4 @@
-package com.rukiasoft.githubfetcher;
+package com.rukiasoft.githubfetcher.ui.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.rukiasoft.githubfetcher.R;
 
 public class GithubListActivity extends AppCompatActivity {
 
