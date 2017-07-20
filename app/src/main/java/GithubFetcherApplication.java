334f636478852d970b64cd87d793f895307e75e7
Kuno Tatemaki <raulfeliz@gmail.com>
2017-07-20 19:20:31 +0200

    add custom app

---
+++ app/src/main/java/GithubFetcherApplication.java
@@ -0,0 +1,15 @@
+import android.app.Application;
+
+/**
+ * Created by Roll on 20/7/17.
+ */
+
+public class GithubFetcherApplication extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
